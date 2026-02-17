92e15a7a92d1095ebe46eb8c1fdb9732df54914e
bayerlcloud <ludwig@bayerlcloud.de>
2026-02-17 15:19:53 +0100

    feat(kohlkopf): integrate monolog-bundle
    - Register MonologBundle in bundles.php
    - Add monolog.yaml config for dev/test/prod environments
    - Update composer.lock with monolog and related packages
    - Update symfony.lock with monolog-bundle recipe

--- apps/kohlkopf/config/bundles.php
+++ apps/kohlkopf/config/bundles.php
@@ -4,6 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     SpomkyLabs\PwaBundle\SpomkyLabsPwaBundle::class => ['all' => true],
     Minishlink\Bundle\WebPushBundle\MinishlinkWebPushBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
